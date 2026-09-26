e179e374cc4e033399d9f445eac5b40985ee2cc1
Joshua Kashambala <joshua.kashambala@gmail.com>
2026-09-25 20:12:31 -0400

    code for effort and encoder tick pairing

--- 01_motor_characterization/src/main.cpp
+++ 01_motor_characterization/src/main.cpp
@@ -5,13 +5,15 @@
 Romi32U4Motors motors;
 Romi32U4Encoders encoders;
 
-unsigned long time{0};
-
   struct countPair {
     int16_t right_count;
     int16_t left_count;
     int effort;
   };
+
+  bool loopDone {false};
+  countPair pairs[5] = {0};
+  int effort_array[5] = {20, 40, 60, 80, 100};
 
 
 
@@ -20,9 +22,6 @@ void setup()
 
   Serial.begin(9600);
 
-
-
-
 }
 
 
@@ -30,25 +29,45 @@ void setup()
 void loop()
 {
 
-  countPair pairs[5] = {0};
-  
-  countPair my_count = {0,0, 12};
 
-  
-  Serial.print("Time: ");
-  time = millis();
+  while(!loopDone) {
 
-  int16_t left_count = encoders.getCountsLeft();
-  int16_t right_count = encoders.getCountsRight();
-
-  delay(100);
-
-  for(int i {0}; i < (sizeof(pairs)/sizeof(pairs[0]))+1; i++) {
+  for(int i {0}; i < (sizeof(pairs)/sizeof(pairs[0])); i++) {
     unsigned long current_time = millis();
+
+    motors.setSpeeds(effort_array[i], effort_array[i]);
+
+    while (((millis() - current_time)/1000) < 10) {}
+
+    countPair current_pair = {encoders.getCountsRight(), encoders.getCountsLeft(), effort_array[i]}; 
+
+    current_time = millis();
+    motors.setSpeeds(0, 0);
+
+    while (((millis() - current_time)/1000) < 5) {}
+    encoders.getCountsAndResetLeft();
+    encoders.getCountsAndResetRight();
+
+    pairs[i] = current_pair;
   }
 
+  loopDone = true;
 
-  
+
+  Serial.println("Left Count  || Right Count || Effort ");
+  for(const auto &value: pairs) {
+
+  Serial.print(value.left_count);
+  Serial.print(" ");
+  Serial.print(value.right_count);
+  Serial.print(" ");
+  Serial.print(value.effort);
+
+}
+
+
+}
+
 
 }
 
